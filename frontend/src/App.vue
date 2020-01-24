1fe5249c605ebb101267568021f2b0be66a336f7
Thiago Traczykowski <thiagotraczykowski@gmail.com>
2020-01-24 03:10:09 -0300

    UPDATE - nome do projeto e msg inicial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app" class="min-h-screen flex items-center justify-center bg-blue-900">
-        <h1 class="text-5xl font-bold text-red-500">Hello from Tailwind.</h1>
+        <h1 class="text-5xl font-bold text-red-500">BURN BABY BURN.</h1>
     </div>
 </template>
 
